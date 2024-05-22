a179261d4d3ce39d9ddb8c8d0fdff0d070cb5b34
Vietsentinel <viettrun2@gmail.com>
2024-05-22 16:35:36 +0700

    -cmd commit

---
+++ WebApiNet5/WebApiNet5/Data/MyDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiNet5.Data
+{
+    public class MyDBContext : DbContext
+    {
+        public  MyDBContext(DbContextOptions options) :base(options)
+        { }
+        #region
+        public DbSet<HangHoa>hangHoas { get; set; }
+       public DbSet<Loai>Loais { get; set; }
+        #endregion
+    }
+}
